2738caeea2e84dd51ce9a71f25ada37f8b8dc067
Adam Yiu <adam_yiu@outlook.com>
2022-08-09 15:40:23 -0400

    Update README

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import Wow from "./Wow"
+import { Fragment } from 'react';
 
 function App() {
 
   return (
-    <div>
+    <Fragment>
        <Wow></Wow>
-    </div>
+    </Fragment>
   );
 }
 
--- src/Wow.js
+++ src/Wow.js
@@ -9,15 +9,9 @@ export default function Button() {
 
   let audio = "";
 
-  // let movieName = "";
-  // let releaseDate = "";
-  // let quote = "";
-  // let director = "";
-
   const generateWow = function () {
     axios.get("https://owen-wilson-wow-api.herokuapp.com/wows/random")
       .then(res => {
-        console.log(res.data[0]);
         setMovieName(res.data[0].movie);
         setReleaseDate(res.data[0].release_date);
         setDirector(res.data[0].director);
@@ -25,8 +19,6 @@ export default function Button() {
         audio = new Audio(res.data[0].audio);
 
         audio.play();
-
-
       })
       .catch(err => {
         console.log(err);
@@ -40,7 +32,6 @@ export default function Button() {
       {(movieName && <h5> You just heard Owen Wilson say 'Wow!' from: {movieName} </h5>)}
       {(movieName && <h5> Directed by: {director} </h5>)}
       {(movieName && <h5> Released in: {releaseDate} </h5>)}
-
     </div>
   )
 }
